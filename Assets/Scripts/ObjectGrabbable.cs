6f108ded083ff4f51bf02b2d34f9a1384379695a
Luke McDougall <mcdougallluke211@gmail.com>
2024-03-26 15:20:22 -0600

    picking up goes to the docs hand

--- Assets/Scripts/ObjectGrabbable.cs
+++ Assets/Scripts/ObjectGrabbable.cs
@@ -7,24 +7,30 @@ public class ObjectGrabbable : MonoBehaviour
 
     private Rigidbody objectRigidbody;
     private Transform objectGrabPointTransform;
+    private Collider collider;
 
     private void Awake() {
         objectRigidbody = GetComponent<Rigidbody>();
+        collider = GetComponent<Collider>();
+
     }
     public void Grab(Transform objectGrabPointTransform) {
         this.objectGrabPointTransform = objectGrabPointTransform;
         objectRigidbody.useGravity = false;
+        collider.enabled = false;
     }
 
     public void Drop() {
         this.objectGrabPointTransform = null;
         objectRigidbody.useGravity = true;
+        collider.enabled = true;
     }
 
     private void FixedUpdate() {
         if (objectGrabPointTransform != null) {
-            Vector3 newPosition = Vector3.Lerp(transform.position, objectGrabPointTransform.position, Time.fixedDeltaTime * 15f);
-            objectRigidbody.MovePosition(newPosition);
+            // Vector3 newPosition = Vector3.Lerp(transform.position, objectGrabPointTransform.position, Time.fixedDeltaTime * 15f);
+            // objectRigidbody.MovePosition(newPosition);
+            objectRigidbody.MovePosition(objectGrabPointTransform.position);
         }
     }
 }
