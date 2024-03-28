30e7ee18f3b038ca965c1d6acfe40df64d47dc23
Ikirochi <danielprairie1@gmail.com>
2024-03-28 17:27:27 -0600

    added a main menu
    added a main menu

---
+++ Assets/Scripts/Daniel/MainMenu.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    /*AudioManager audioManager;
+    private void Awake()
+    {
+        audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
+    }*/
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Credits()
+    {
+        Debug.Log("Going to credits scene.");
+        //SceneManager.LoadScene(2);
+    }
+    public void QuitGame()
+    { 
+        Application.Quit();
+        Debug.Log("Quiting Game.");
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
