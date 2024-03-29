151df552c524746668edbf5cbd6e0d999972d7f3
Jack Connor Vanlyssel <jackvanlyssel@gmail.com>
2024-03-28 21:22:51 -0600

    adding speech features

---
+++ Assets/Thirdparty/NPC/Speech/NPCInteractable.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCInteractable : MonoBehaviour, IIInteractable {
+
+    [SerializeField] private string interactText;
+
+    private Animator animator;
+
+    private void Awake() {
+        animator = GetComponent<Animator>();
+    }
+
+    public void Interact(Transform interactorTransform) {
+        ChatBubble3D.Create(transform.transform, new Vector3(-.3f, 1.7f, 0f), ChatBubble3D.IconType.Happy, "Hello there!");
+
+        //animator.SetTrigger("Talk");
+    }
+
+    public string GetInteractText() {
+        return interactText;
+    }
+
+    public Transform GetTransform() {
+        return transform;
+    }
+
+}
