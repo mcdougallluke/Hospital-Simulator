0e3f8763b76d33c2ff9bb0611fdf0855c0b7f491
Ikirochi <danielprairie1@gmail.com>
2024-04-13 12:27:37 -0600

    volume settings
    volume settings

---
+++ Assets/Scripts/Daniel/VolumeSettings.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+public class VolumeSettings : MonoBehaviour
+{
+    [SerializeField] private AudioMixer myMixer;
+    [SerializeField] private Slider musicSlider;
+    [SerializeField] private Slider SFXSlider;
+    void Start()
+    {
+        SetMusicVolume();
+        SetSFXVolume();
+    }
+
+    public void SetMusicVolume()
+    {
+        float volume = musicSlider.value;
+        myMixer.SetFloat("Background Music", Mathf.Log10(volume)*20);
+    }
+    public void SetSFXVolume()
+    {
+        float volume = SFXSlider.value;
+        myMixer.SetFloat("SFX", Mathf.Log10(volume) * 20);
+    }
+}
