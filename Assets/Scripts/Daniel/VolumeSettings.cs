71ecb2f0c0724f252fab85323907fce67efecbfc
Ikirochi <danielprairie1@gmail.com>
2024-04-13 12:33:37 -0600

    volume settings implemented
    volume settings implemented

--- Assets/Scripts/Daniel/VolumeSettings.cs
+++ Assets/Scripts/Daniel/VolumeSettings.cs
@@ -10,18 +10,42 @@ public class VolumeSettings : MonoBehaviour
     [SerializeField] private Slider SFXSlider;
     void Start()
     {
-        SetMusicVolume();
-        SetSFXVolume();
+        if (PlayerPrefs.HasKey("musicVolume"))
+        {
+            LoadVolume();
+        }
+        else
+        {
+            SetMusicVolume();
+        }
+        if (PlayerPrefs.HasKey("SFXVolume"))
+        {
+            LoadVolume();
+        }
+        else
+        {
+            SetSFXVolume();
+        }
     }
 
     public void SetMusicVolume()
     {
         float volume = musicSlider.value;
         myMixer.SetFloat("Background Music", Mathf.Log10(volume)*20);
+        PlayerPrefs.SetFloat("musicVolume", volume);
     }
     public void SetSFXVolume()
     {
         float volume = SFXSlider.value;
         myMixer.SetFloat("SFX", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("SFXVolume", volume);
+    }
+
+    private void LoadVolume()
+    {
+        musicSlider.value = PlayerPrefs.GetFloat("musicVolume");
+        SFXSlider.value = PlayerPrefs.GetFloat("SFXVolume");
+        SetMusicVolume();
+        SetSFXVolume();
     }
 }
