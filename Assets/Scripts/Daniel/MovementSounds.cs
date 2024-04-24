6e779eeedb6f8d775a4411656b07bf40e9c5ef6e
Ikirochi <danielprairie1@gmail.com>
2024-04-24 14:30:47 -0600

    running and walking sounds
    running and walking sounds

---
+++ Assets/Scripts/Daniel/MovementSounds.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class footsteps : MonoBehaviour
+{
+    public AudioSource footstepsSound, sprintSound;
+    void Update()
+    {
+        footstepsSound.volume = 0.2f;
+        sprintSound.volume = 0.2f;
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                footstepsSound.enabled = false;
+                sprintSound.enabled = true;
+            }
+            else
+            {
+                footstepsSound.enabled = true;
+                sprintSound.enabled = false;
+            }
+        }
+        else
+        {
+            footstepsSound.enabled = false;
+            sprintSound.enabled = false;
+        }
+    }
+}
